4fc00833598ffd6e38f4b412a2afd25bc96d0894
veerapuraju <116946808+veerapuraju@users.noreply.github.com>
2025-09-25 12:23:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Typing Effect
+const texts = [
+  "B.Tech CSE (AI/ML) @ GITAM",
+  "AI/ML Engineer 🚀",
+  "Software Engineer 💻",
+  "Programmer | Problem Solver ⚡"
+];
+let count = 0, index = 0, currentText = "", letter = "";
+
+(function type() {
+  if (count === texts.length) count = 0;
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+  document.getElementById("typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1000);
+  } else {
+    setTimeout(type, 120);
+  }
+})();
